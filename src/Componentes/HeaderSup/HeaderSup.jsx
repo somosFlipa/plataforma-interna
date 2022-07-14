5c1ccef973af2afe6e05ed612c68453d60cd2ca6
ManuelMU <manumerino01@gmail.com>
2022-07-14 10:10:40 -0300

    Buscardor y maquetado sin css

--- src/Componentes/HeaderSup/HeaderSup.jsx
+++ src/Componentes/HeaderSup/HeaderSup.jsx
@@ -4,16 +4,29 @@ import Logo from "../../Media/PlanerEAT.svg"
 import "./HeaderSup.css"
 
 function HeaderSup() {
-    
+    document.addEventListener("keyup", e=>{
+        
+        if (e.target.matches("#buscador")){  //True o false si identifica el keyup
+      
+            if (e.key ==="Escape")e.target.value = ""
+      
+            document.querySelectorAll(".ingredientes").forEach(ingre =>{
+      
+                ingre.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+                  ?ingre.classList.remove("filtro") :ingre.classList.add("filtro")
+            });
+            //console.log(e.target.value)
+        }
+    });
 
   return (
     <>
-        <div>
-            <div>
+        <div className='cont-header-sup'>
+            <div className='logo'>
                 <img src={Logo} alt='Logo PlannerEat'/>
             </div>
 
-            <div>
+            <div className='nav-buscador'>
                 <input type="text" name="buscador" id="buscador" placeholder="🔍 Buscar"/>
             </div>
         </div>
